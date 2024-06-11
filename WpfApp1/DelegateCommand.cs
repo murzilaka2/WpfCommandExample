bcc60680f217d5f62d566bf70c8c7c1ef69e87b4
enyko <enyko@LAPTOP-O3E2VRHG>
2024-06-11 18:06:17 +0300

    Add project files.

---
+++ WpfApp1/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    sealed class DelegateCommand : Command
+    {
+        private static readonly Func<bool> defaultCanExecuteMethod = () => true;
+        private readonly Func<bool> canExecuteMethod;
+        private readonly Action executeMethod;
+        public DelegateCommand(Action executeMethod) : this(executeMethod, defaultCanExecuteMethod)
+        {
+        }
+        public DelegateCommand(Action executeMethod, Func<bool> canExecuteMethod)
+        {
+            this.canExecuteMethod = canExecuteMethod;
+            this.executeMethod = executeMethod;
+        }
+        protected override bool CanExecute()
+        {
+            return canExecuteMethod();
+        }
+        protected override void Execute()
+        {
+            executeMethod();
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new DataSource();
+        }
+    }
+    class DataSource : INotifyPropertyChanged
+    {
+        private readonly Command blueCommand;
+        private readonly Command greenCommand;
+        private readonly Command redCommand;
+        private string selectedColor = "Black";
+        public DataSource()
+        {
+            blueCommand = new DelegateCommand(
+            () => SelectedColor = "Blue",
+            () => SelectedColor != "Blue"
+            );
+            greenCommand = new DelegateCommand(() => SelectedColor = "Green", () => SelectedColor != "Green");
+            redCommand = new DelegateCommand(
+            () => SelectedColor = "Red",
+            () => SelectedColor != "Red"
+            );
+            PropertyChanged += (sender, e) =>
+            {
+                if (e.PropertyName.Equals(nameof(SelectedColor)))
+                {
+                    blueCommand.RaiseCanExecuteChanged();
+                    greenCommand.RaiseCanExecuteChanged();
+                    redCommand.RaiseCanExecuteChanged();
+                }
+            };
+        }
+        public ICommand BlueCommand => blueCommand;
+        public ICommand GreenCommand => greenCommand;
+        public ICommand RedCommand => redCommand;
+        public string SelectedColor
+        {
+            get => selectedColor;
+            set
+            {
+                if (!selectedColor.Equals(value))
+                {
+                    selectedColor = value;
+                    OnPropertyChanged(new PropertyChangedEventArgs(nameof(SelectedColor)));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChanged?.Invoke(this, e);
+        }
+    }
+}
